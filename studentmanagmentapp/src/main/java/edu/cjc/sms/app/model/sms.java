fe3212cd8811f4f41b07e50848797932f475eb5b
Audumbar <Audumbar@LAPTOP-59JC22CL>
2025-12-15 11:24:49 +0530

    sms class created

---
+++ studentmanagmentapp/src/main/java/edu/cjc/sms/app/model/sms.java
@@ -0,0 +1,16 @@
+package edu.cjc.sms.app.model;
+
+public class sms {
+
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
