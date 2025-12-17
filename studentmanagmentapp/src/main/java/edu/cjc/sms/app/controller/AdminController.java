c49318bb5b17f4ef76869df188d916e1e0f38e83
Audumbar <Audumbar@LAPTOP-59JC22CL>
2025-12-17 09:40:45 +0530

    Add Second Day Work

---
+++ studentmanagmentapp/src/main/java/edu/cjc/sms/app/controller/AdminController.java
@@ -0,0 +1,29 @@
+package edu.cjc.sms.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AdminController {
+
+	@RequestMapping("/")
+	public String preLogin()
+	{
+		return "login";
+	}
+	
+	@RequestMapping("/login")
+	public String onLogin(@RequestParam String username, @RequestParam String password, Model m) {
+		
+		if(username.equals("admin") && password.equals("admin"))
+		{
+			return"adminscreen";
+		}else 
+		{
+			m.addAttribute("login_fail","Enter valid login details..");
+			return "login";
+		}
+	}
+}
